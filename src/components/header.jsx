84f6b24a59ee615bcd01aa9a019c7bc0fe66f5d6
0xcodercrane <codercrane@gmail.com>
2023-12-02 19:04:21 +0900

    header search

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,13 +11,13 @@ export default function Header() {
           <li className="cursor-pointer">Inscribe</li>
           <li className="cursor-pointer">Wallet</li>
         </ul>
-        <div className="sm:flex hidden  gap-2 hover:shadow hover:bg-white/30 px-3 py-1 hover:shadow-black backdrop-filter rounded-xl bg-main border border-transparent">
+        {/* <div className="sm:flex hidden  gap-2 hover:shadow hover:bg-white/30 px-3 py-1 hover:shadow-black backdrop-filter rounded-xl bg-main border border-transparent">
           <IoSearch className="text-2xl my-auto" />
           <input
             type="text"
             className="focus-visible:outline-none transition ease-in-out bg-transparent text-white hover:border-secondary"
           />
-        </div>
+        </div> */}
 
         <button className="rounded-lg bg-main px-3 py-2 font-semibold flex gap-2 items-center hover:shadow hover:shadow-black hover:bg-white/30 transition ease-in-out duration-300">
           <FaWallet className="text-2xl my-auto" /> Connect Wallet
