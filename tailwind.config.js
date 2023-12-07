4ec6615223e71f780c91d2bc979b2ffaf6a9b8ef
0xcodercrane <codercrane@gmail.com>
2023-12-07 12:56:02 +0900

    new version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/layout/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#eabe45",
-        secondary: "#53483f",
-        br: "#3b3b3b",
-        blue: "#6389dc"
-      }
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: []
 }
