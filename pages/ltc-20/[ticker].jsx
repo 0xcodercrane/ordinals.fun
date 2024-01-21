ba72c972e7c7eb5c1015583a82f0b3703f5ec45a
0xcodercrane <codercrane@gmail.com>
2024-01-22 03:52:09 +0900

    12

--- pages/ltc-20/[ticker].jsx
+++ pages/ltc-20/[ticker].jsx
@@ -147,7 +147,7 @@ export default function Collection() {
           ) : (
             <>
               <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-2 lg:gap-4 w-full relative">
-                {Array.from({ length: 6 }, (_, index) => {
+                {Array.from({ length: 12 }, (_, index) => {
                   return <LTCBuyCardSkelenton key={index} />;
                 })}
 
