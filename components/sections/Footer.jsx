0f976d1cf2c8337c16c7358646c3806bdb71c288
nextidearly <talenteddev0125@gamil.com>
2024-02-09 16:03:39 +0900

    contact dev

--- components/sections/Footer.jsx
+++ components/sections/Footer.jsx
@@ -9,22 +9,29 @@ import {
 
 export default function Footer() {
   return (
-    <div className="mt-8 container flex justify-center items-center gap-3 absolute bottom-3 left-0 w-full">
-      <button className="rounded-full h-[40px] w-[40px] text-xl bg-[#216da5] shadow shadow-black flex justify-center items-center">
-        <FaDiscord />
-      </button>
-      <button className="rounded-full h-[40px] w-[40px] text-xl bg-[#216da5] shadow shadow-black flex justify-center items-center">
-        <FaTwitter />
-      </button>
-      <button className="rounded-full h-[40px] w-[40px] text-xl bg-[#216da5] shadow shadow-black flex justify-center items-center">
-        <FaTelegram />
-      </button>
-      <button className="rounded-full h-[40px] w-[40px] text-xl bg-[#216da5] shadow shadow-black flex justify-center items-center">
-        <FaYoutube />
-      </button>
-      <button className="rounded-full h-[40px] w-[40px] text-xl bg-[#216da5] shadow shadow-black flex justify-center items-center">
-        <FaMedium />
-      </button>
+    <div className="mt-8 container absolute bottom-3 left-0 w-full">
+      <div className="flex justify-center items-center gap-3 ">
+        <button className="rounded-full h-[40px] w-[40px] text-xl bg-[#216da5] shadow shadow-black flex justify-center items-center">
+          <FaDiscord />
+        </button>
+        <button className="rounded-full h-[40px] w-[40px] text-xl bg-[#216da5] shadow shadow-black flex justify-center items-center">
+          <FaTwitter />
+        </button>
+        <button className="rounded-full h-[40px] w-[40px] text-xl bg-[#216da5] shadow shadow-black flex justify-center items-center">
+          <FaTelegram />
+        </button>
+        <button className="rounded-full h-[40px] w-[40px] text-xl bg-[#216da5] shadow shadow-black flex justify-center items-center">
+          <FaYoutube />
+        </button>
+        <button className="rounded-full h-[40px] w-[40px] text-xl bg-[#216da5] shadow shadow-black flex justify-center items-center">
+          <FaMedium />
+        </button>
+      </div>
+      <div className="w-full flex justify-end mt-1">
+        <a href="https://t.me/genius021120" target="_blank">
+          @Contact The Dev
+        </a>
+      </div>
     </div>
   );
 }
